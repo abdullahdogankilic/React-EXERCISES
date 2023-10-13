01982c5fb8f297dd24238dec784a4e9a1bb54363
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-13 22:02:52 +0300

    16

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import Welcome from "./Welcome";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <div>
+      <Welcome name="Ahmet" />
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Welcome = ({ name }) => {
+  return <p>Hoşgeldiniz, {name}!</p>;
+};
+
+export default Welcome;
