165e5070bc8311e98844e1179be647144624ce71
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-14 20:56:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <div>
+      <h2> {count} </h2>
+      <button onClick={() => setCount(count + 1)}>Click </button>
+    </div>
   );
 }
-
 export default App;
