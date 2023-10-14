4b153beeb12002146f493a57acfb4f4c91a5314f
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-14 21:05:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const Counter = ({ Value, increment }) => {
+  const [count, setCount] = useState(Value);
 
   return (
     <div>
-      <h2> {count} </h2>
-      <button onClick={() => setCount(count + 1)}>Click </button>
+      <h2>{count}</h2>
+      <button onClick={setCount(count + increment)}>Click</button>
     </div>
   );
-}
-export default App;
+};
 
-//  If you pass a value directly, such as setCounter(counter + 1),
-//  React's ability to keep track of condition conditions can be detected in these differences.
-//   However, in accordance with its function,
-//   it ensures that every update step of React is followed accurately.
+export default Counter;
