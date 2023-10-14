194caf1fdfe57c7348d4919d8b7b04eb5dcaa48f
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-14 19:14:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 
 const Age = (props) => {
-  return <div>Your age is {props.age}.</div>;
+  const { age } = props;
+  return (
+    <div>
+      {age > 18 ? <p>Your age is {age}.</p> : <p>You are very young!</p>}
+    </div>
+  );
 };
 
 const Welcome = (props) => {
   return (
     <div>
       {props.name == "John" && <h1>Welcome, {props.name}!</h1>}
-      {props.age > 18 && props.age < 65 && <Age age={props.age} />}
+      <Age age={props.age} />
     </div>
   );
 };
 
 const App = () => {
   const name = "John";
-  const age = 50;
+  const age = 15;
 
   return <Welcome name={name} age={age} />;
 };
