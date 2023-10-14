38209c4865d30c8568246bdd8dc85a513bd0e162
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-14 21:18:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import CounterDisplay from "./Counter";
 
-const App = ({ Value, increment }) => {
+const App = ({ Value, increment, decrement }) => {
   const [count, setCount] = useState(Value);
 
   return (
     <div>
       <CounterDisplay count={count} />
       <button onClick={setCount(count + increment)}>Click</button>
+      <button onClick={setCount(count - decrement)}>decrement</button>
+      <button onClick={setCount(Value)}>Reset</button>
     </div>
   );
 };
