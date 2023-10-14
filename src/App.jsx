5d654f34092d9be36def15c046c5de4820a2722d
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-14 18:58:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const Welcome = (props) => {
   return (
     <div>
       <h1>Welcome, {props.name}!</h1>
-      {props.age > 18 && props.age < 65 && <Age age={props.age} />}
+      {props.age && <Age age={props.age} />}
     </div>
   );
 };
