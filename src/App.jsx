f1db85fbfd4f2c67c1ec16372b300301158e5479
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-17 14:23:21 +0300

    7

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const App = () => {
-  return <h2>Hello, World!</h2>;
-};
+function Hello() {
+  return (
+    <div>
+      <h2>Hello, World!</h2>
+    </div>
+  );
+}
 
-export default App;
+export default Hello;
