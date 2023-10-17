09cc9ea1e3c17c414403238695e6fec820b21450
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-17 15:41:46 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <h2> {count} </h2>
-      <button onClick={() => setCount(count + 1)}>Click </button>
+      <Counter />
     </div>
   );
 }
