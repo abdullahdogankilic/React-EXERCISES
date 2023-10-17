fa2243e8c5d8944df5a875e70f28d1e61e51d094
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-17 16:22:34 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from "react";
+import Clock from "./Clock";
 
 function App() {
-  const [Time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-  }, []);
   return (
     <>
-      <div>
-        <h2>{Time.toLocaleTimeString()}</h2>
-      </div>
+      <Clock />
     </>
   );
 }
