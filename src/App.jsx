4ac4b5c9ac2f1d9fc4b5d400ca8ba4173443b861
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-17 16:39:17 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,8 @@
 import React, { useState } from "react";
+import Login from "./Login";
 
-function Login({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [remember, setRemember] = useState(false);
-
-  function Username(event) {
-    setUsername(event.target.value);
-  }
-
-  function Password(event) {
-    setPassword(event.target.value);
-  }
-
-  function Remember(event) {
-    setRemember(event.target.checked);
-  }
-  function handleLogin() {
-    if (username && password) {
-      onLogin({ username, password, remember });
-    }
-  }
-
-  const Disabled = !username || !password;
-
-  return (
-    <div>
-      <div>
-        <label>Username:</label>
-        <input type="text" onChange={Username} />
-      </div>
-
-      <div>
-        <label>Password:</label>
-        <input type="password" onChange={Password} />
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" onChange={Remember} />
-          Remember
-        </label>
-        <button onClick={handleLogin} disabled={Disabled}>
-          Login
-        </button>
-      </div>
-    </div>
-  );
+function App() {
+  return <Login />;
 }
 
-export default Login;
+export default App;
