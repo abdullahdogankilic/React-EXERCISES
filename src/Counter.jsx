115556062a0fa6ef8b48e7beb6b2c9eded00d64f
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-17 15:46:17 +0300

    2

---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2> {count} </h2>
+      <button onClick={() => setCount(count + 1)}>Click </button>
+    </div>
+  );
+}
+export default Counter;
+
+//  If you pass a value directly, such as setCounter(counter + 1),
+//  React's ability to keep track of condition conditions can be detected in these differences.
+//   However, in accordance with its function,
+//   it ensures that every update step of React is followed accurately.
