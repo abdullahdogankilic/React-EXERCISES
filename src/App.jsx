b17c1186879bc397493dc999ea0f1afec13a34c8
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-17 21:18:53 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Counter from "./Counter";
 function App() {
   return (
     <div>
-      <CounterDisplay Value={2} increment={2} decrement={2} />
+      <Counter Value={2} increment={2} decrement={2} />
     </div>
   );
 }
