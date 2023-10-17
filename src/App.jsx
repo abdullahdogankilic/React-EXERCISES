d48f7b91ee7f5cdda4380eeaff2968484407d831
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-17 15:19:05 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import React from "react";
+import Welcome from "./Welcome";
 
-const Age = (props) => {
-  const { age } = props;
-  return (
-    <div>
-      {age > 18 ? <p>Your age is {age}.</p> : <p>You are very young!</p>}
-    </div>
-  );
-};
-
-const Welcome = (props) => {
-  return (
-    <div>
-      {props.name == "John" && <h1>Welcome, {props.name}!</h1>}
-      <Age age={props.age} />
-    </div>
-  );
-};
-
-const App = () => {
+function App() {
   const name = "John";
   const age = 15;
 
   return <Welcome name={name} age={age} />;
-};
+}
 export default App;
