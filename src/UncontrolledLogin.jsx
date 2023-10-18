68b36afc832183c33689c4e4ce01bddd6217b0ee
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-18 15:08:42 +0300

    Update UncontrolledLogin.jsx

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -1,51 +1,40 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
-function UncontrolledLogin({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+function Login({ onLogin }) {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+  const rememberRef = useRef(null);
 
-  function handleLogin(event) {
-    const username = event.target.elements.username.value;
-    const password = event.target.elements.password.value;
-    const remember = event.target.elements.remember.checked;
-    event.preventDefault();
+  function handleLogin() {
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    const remember = rememberRef.current.checked;
+
     if (username && password) {
       onLogin({ username, password, remember });
     }
   }
 
-  const Disabled = !username || !password;
-
   return (
-    <form onSubmit={handleLogin}>
+    <div>
       <div>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" name="remember" />
-            Remember
-          </label>
-          <button disabled={Disabled}>Login</button>
-        </div>
+        <label>Username:</label>
+        <input type="text" ref={usernameRef} />
       </div>
-    </form>
+
+      <div>
+        <label>Password:</label>
+        <input type="password" ref={passwordRef} />
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" ref={rememberRef} />
+          Remember
+        </label>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </div>
   );
 }
 
-export default UncontrolledLogin;
+export default Login;
