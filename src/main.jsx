56ede1c22f542207f9e8265f02c355cba4a9ac52
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-18 15:15:50 +0300

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Hello from "./App.jsx";
-import exes from "./exe.jsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Hello />
-    <exes />
   </React.StrictMode>
 );
