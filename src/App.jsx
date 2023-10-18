6210b57cd234122d2f52c1130428cd57f142f88e
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-18 19:48:18 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import Message from "./message";
+import Hello from "./Hello";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <Hello /> <Hello />
       </div>
+      <Message />
     </>
   );
 }
-
 export default App;
+
+// it will not be a problem at all
