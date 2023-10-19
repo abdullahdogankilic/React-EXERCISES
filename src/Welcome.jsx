8ce5ebf87721f431537ea8f1aed5d9bfa7cb69f1
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-19 17:01:29 +0300

    2

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Age from "./Age";
 
-export default function Welcome({ name, age }) {
+export default function Welcome(props) {
+  const { name, age } = props;
+
   return (
     <div>
       <h1>Welcome, {name}!</h1>
-      {age > 18 && <Age age={age} />}
+      {age && <Age age={age} />}
     </div>
   );
 }
