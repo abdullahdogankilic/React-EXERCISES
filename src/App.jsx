6a6e496c59a5e4369c6e40d5340b2069ffdce2f1
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-20 00:40:50 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Container from "./Container";
 import "./App.css";
+import LanguageProvider from "./LanguageContext";
+import Clock from "./Clock";
 
 function App() {
   return (
     <div>
+      <LanguageProvider>
+        <Clock />
+      </LanguageProvider>
       <Container title={<h1>My Awesome Application</h1>}>
         <h1>Hello world!</h1>
       </Container>
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { LanguageContext } from "./LanguageContext";
 
 function Clock() {
   const [Time, setTime] = useState(new Date());
+  const { language, setLanguage } = useContext(LanguageContext);
+
+  const handleLanguageChange = (e) => {
+    setLanguage(e.target.value);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -10,8 +16,22 @@ function Clock() {
   }, []);
   return (
     <>
+      <div>{/* <h2>{Time.toLocaleTimeString()}</h2> */}</div>
       <div>
-        <h2>{Time.toLocaleTimeString()}</h2>
+        <select onChange={handleLanguageChange}>
+          <option value="en">English</option>
+          <option value="fr">French</option>
+          <option value="es">Spanish</option>
+        </select>
+        <div>
+          {language === "en" && (
+            <p>Current Time: {Time.toLocaleTimeString()}</p>
+          )}
+          {language === "fr" && (
+            <p>Heure actuelle: {Time.toLocaleTimeString()}</p>
+          )}
+          {language === "es" && <p>Hora actual: {Time.toLocaleTimeString()}</p>}
+        </div>
       </div>
     </>
   );
