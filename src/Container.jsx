78fdce52f3184152d89485b4ab890715f611aca8
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-20 00:08:20 +0300

    2

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 const Container = ({ children, title }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  function handleToggleCollapse() {
+    setCollapsed((t) => !t);
+  }
   return (
     <div className="container">
-      <div className="app-title">{title}</div>
-      <div className="app-content">{children}</div>
+      <div className="app-title">
+        {title} <button onClick={handleToggleCollapse}>Toggle</button>
+      </div>
+      <div className={!collapsed ? "app-content" : "app-content-hidden"}>
+        {children}
+      </div>
     </div>
   );
 };
