f2462575d85dd1c20a1a2d1e79683e563a09db64
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-19 22:38:09 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
-import Login from "./Login";
+import React from "react";
+import Container from "./Container";
+import "./App.css";
 
 function App() {
-  return <Login />;
+  return (
+    <div>
+      <Container>
+        <h1>Hello world!</h1>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Container.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./App.css";
+const Container = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
+
+export default Container;
