8fda0367617a6826bbae89fe7783fd5ca571c11c
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-19 20:51:19 +0300

    Update InteractiveWelcome .jsx

--- src/InteractiveWelcome .jsx
+++ src/InteractiveWelcome .jsx
@@ -4,13 +4,13 @@ import React, { useState } from "react";
 function InteractiveWelcome() {
   const [inputValue, setInputValue] = useState("");
 
-  function Input(event) {
+  function input(event) {
     setInputValue(event.target.value);
   }
 
   return (
     <div>
-      <input type="text" onChange={Input} placeholder="Enter your name" />
+      <input type="text" onChange={input} placeholder="Enter your name" />
       <Welcome name={inputValue} />
     </div>
   );
