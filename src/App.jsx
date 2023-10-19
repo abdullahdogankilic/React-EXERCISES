3f52e3d5b896dcd4a80f446706e752d75e05dd51
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-20 01:12:04 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-import React, { useState } from "react";
-import Container from "./Container";
+import React from "react";
 import "./App.css";
-import { LanguageContext } from "./LanguageContext";
+import { LanguageProvider } from "./LanguageContext";
 import Clock from "./Clock";
 
 function App() {
-  const [language, setLanguage] = useState("en");
-
-  const handleLanguageChange = (e) => {
-    setLanguage(e.target.value);
-  };
-
   return (
     <div>
-      <div>
-        <select onChange={handleLanguageChange}>
-          <option value="en">English</option>
-          <option value="it">Italian</option>
-        </select>
-      </div>
-      <LanguageContext.Provider value={{ language, setLanguage }}>
-        <Clock language={language} />
-      </LanguageContext.Provider>
-      <Container title={<h1>My Awesome Application</h1>}>
-        <h1>Hello world!</h1>
-      </Container>
+      <LanguageProvider>
+        <div>
+          <Clock />
+        </div>
+      </LanguageProvider>
     </div>
   );
 }
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,19 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { LanguageContext } from "./LanguageContext";
 
-function Clock({ language }) {
-  const [Time, setTime] = useState(new Date());
+const Clock = () => {
+  const { language, setLanguage } = useContext(LanguageContext);
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date());
     }, 1000);
+    return () => clearInterval(interval);
   }, []);
+
+  const handleLanguageChange = (e) => {
+    setLanguage(e.target.value);
+  };
+
   return (
-    <>
-      <div>{/* <h2>{Time.toLocaleTimeString()}</h2> */}</div>
-      <div></div>
-    </>
+    <div>
+      <select value={language} onChange={handleLanguageChange}>
+        <option value="en">English</option>
+        <option value="it">Italy</option>
+        <option value="es">Spanish</option>
+      </select>
+    </div>
   );
-}
+};
 
 export default Clock;
