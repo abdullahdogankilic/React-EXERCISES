c71ff5892b987b06e5642cc963fc1d80ef54390a
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-20 23:39:06 +0300

    2

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from "react";
 
-const GithubUser = ({ username }) => {
-  const [user, setUser] = useState(null);
-
-  async function fetchGithubUser() {
+function useGithubUser(username) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  async function fetchGithubUser(username) {
+    setLoading(true);
+    setError(null);
     try {
       const response = await fetch(`https://api.github.com/users/${username}`);
       const data = await response.json();
-      setUser(data);
-      console.log(data);
+      setData(data);
     } catch (error) {
-      console.error("Error fetching data from Github API", error);
+      setError(error);
+      setData(null);
+    } finally {
+      setLoading(false);
     }
   }
   useEffect(() => {
     fetchGithubUser();
   }, [username]);
 
+  return { data, loading, error };
+}
+
+function GithubUser({ username }) {
   return (
     <div>
-      {user ? (
-        <div>
-          <h2>Name: {user.name}</h2>
-          <p>Login: {user.login}</p>
-          <img src={user.avatar_url} alt="User Avatar" />
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div>
+        {loading && <p>Loading...</p>}
+        {error && <h1> There has been an error</h1>}
+        {data && <h1> {data.name}</h1>}
+      </div>
     </div>
   );
-};
+}
 
 export default GithubUser;
