32ebac1d4ac19460cbeaa0ed19aca556506d78d5
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-22 21:44:25 +0300

    2

--- src/Color.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Color({ name, id }) {
-  return <li key={id}>{name}</li>;
-}
--- src/Colors.jsx
+++ src/Colors.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Color from "./Color";
 
 export default function Colors({ colors }) {
   return (
     <div>
       <ul>
         {colors.map((color) => (
-          <Color name={color.name} id={color.id} />
+          <li key={color.id}>{color.name}</li>
         ))}
       </ul>
     </div>
