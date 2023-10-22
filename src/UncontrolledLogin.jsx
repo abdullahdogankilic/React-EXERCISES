9d0cfaf1b57da7572060e7de6153b738ec0f329b
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-22 13:42:58 +0300

    Create UncontrolledLogin.jsx

---
+++ src/UncontrolledLogin.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Uncontrolled() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const username = event.target.elements.namedItem("username").value;
+    const password = event.target.elements.namedItem("password").value;
+    const session = event.target.elements.namedItem("session").value;
+
+    const data = {
+      username,
+      password,
+      session,
+    };
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input name="username" />
+        <input type="password" name="password" />
+        <input type="checkbox" name="session" />
+        <button>login </button>
+        <button type="reset">Reset</button>
+      </form>
+    </div>
+  );
+}
+
+export default Uncontrolled;
