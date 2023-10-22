2a5ce626ad20af17810673467e0548a2be4ed3f2
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-22 20:14:49 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Counter from "./Counter";
+import FocusableInput from "./FocusableInput";
 
 function App() {
   return (
     <div>
-      <Counter Value={2} increment={5} decrement={2} />
+      <FocusableInput />
     </div>
   );
 }
