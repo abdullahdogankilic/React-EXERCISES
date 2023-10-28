14e4874d016fb971f65f0fcb927749660aa85c20
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-10-28 20:47:09 +0300

    2

--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -4,7 +4,7 @@ const fetcher = (url) => fetch(url).then((response) => response.json());
 
 const useGithubUser = (username) => {
   const { data, error } = useSWR(
-    username ? `https://api.github.com/users/${username}` : null,
+    `https://api.github.com/users/${username}`,
     fetcher
   );
 
